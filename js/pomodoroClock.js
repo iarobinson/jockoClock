2b0d37739c975e36acc112ad5cda2aa88c199ca5
Ian <ian (at) ianrobinson.net>
2017-12-02 13:11:22 -0800

    Add countdown clock

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -6,7 +6,23 @@ function startClock() {
 }
 
 $(document).ready(function() {
-  $('.initial').on('click', function() {
-    $('.initial').text('JS functioning');
-  });
+  // Source w3 Schools https://www.w3schools.com/howto/howto_js_countdown.asp
+  var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+
+  var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("timeDisplay").innerHTML = days + "d " + hours + "h "
+    + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+      clearInterval(x);
+      document.getElementById("demo").innerHTML = "EXPIRED";
+    }
+  }, 1000);
 });
