3d6a903fb9d3a8d95137fc937d23d7561b58b5a8
Ian <ian (at) ianrobinson.net>
2018-01-26 11:38:52 -0800

    Remove Hilios's open source stuff as it's confusing. Centered and simplified project

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -1,28 +1,19 @@
+// function startClock() {
+//   console.log('Clock Started');
+// }
 
- console.log('pomodoroClock.js is playing');
+var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
 
-function startClock() {
-  console.log('Clock Started');
-}
+var x = setInterval(function() {
+  var now = new Date().getTime();
+  var distance = now - Math.pow(1.5, 6);
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-$(document).ready(function() {
-  // Source w3 Schools https://www.w3schools.com/howto/howto_js_countdown.asp
-  var countDownDate = new Date("Sep 5, 2018 15:37:25").getTime();
+  document.getElementById("timeDisplay").innerHTML = minutes + "m " + seconds + "s ";
 
-  var x = setInterval(function() {
-    var now = new Date().getTime();
-    var distance = countDownDate - now;
-    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-    document.getElementById("timeDisplay").innerHTML = days + "d " + hours + "h "
-    + minutes + "m " + seconds + "s ";
-
-    if (distance < 0) {
-      clearInterval(x);
-      document.getElementById("demo").innerHTML = "EXPIRED";
-    }
-  }, 1000);
-});
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "EXPIRED";
+  }
+}, 1000);
