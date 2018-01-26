b493236d7afb1e8685284fac5ee7882109b946f4
Ian <ian (at) ianrobinson.net>
2018-01-26 14:31:56 -0800

    Add audio files for loading and completing pomodoro period

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -1,19 +1,43 @@
+// Find the day to display on watch
 var d = new Date()
 var days = "SUN MON TUE WED THR FRI SAT".split(' ');
 var day = days[d.getDay()];
-var secondsToGo = 1200;
 
-console.log(day);
+// The number of seconds left on the pomodoro
+var secondsToGo = 12;
 
-var x = setInterval(function() {
-  document.getElementById("timeDisplay").innerHTML = Math.floor((secondsToGo / 60)) + "m " + (secondsToGo % 60) + "s";
-
-  document.getElementById("dayDisplay").innerHTML = day;
-
-  secondsToGo -=1;
+// Toggles Reset button
+$('document').ready(function () {
+  // Displays time and counts down the secondsToGo
+  var x = setInterval(function() {
+    document.getElementById("dayDisplay").innerHTML = day;
+    document.getElementById("timeDisplay").innerHTML = Math.floor((secondsToGo / 60)) + ":" + displaySeconds(secondsToGo);
+    
+    secondsToGo -=1;
+    
+    if (secondsToGo < 0) {
+      clearInterval(x);
+      // document.getElementById("warpath").play();
+      document.getElementById("timeDisplay").innerHTML = "<p id='expired'>EXPIRED<p>";
+      document.getElementById("dayDisplay").innerHTML = '';
+    }
+  }, 1000);
   
-  if (secondsToGo < 0) {
-    clearInterval(x);
-    document.getElementById("timeDisplay").innerHTML = "<p id='expired'>EXPIRED<p>";
+  // Resets clock to 1500 seconds when button clicked
+  $("#resetClock").click(function() {
+    secondsToGo = 1500;
+    return x;
+  });
+})
+
+// Displays 00 rather than 01 for seconds
+function displaySeconds(seconds) {
+  var sparSeconds = seconds % 60;
+  if (sparSeconds === 0) {
+    return '00';
+  } else if (sparSeconds < 10) {
+    return '0' + seconds % 60;
+  } else {
+    return sparSeconds;
   }
-}, 1000);
+}
