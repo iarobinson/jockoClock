61af424c0c8f2dc921aa728e9788fc399c6dca67
Ian <ian (at) ianrobinson.net>
2018-01-26 12:57:33 -0800

    Add Jocko Willink's watch to background and roughly formatted countdown to background

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -1,7 +1,14 @@
-var secondsToGo = 60;
+var d = new Date()
+var days = "SUN MON TUE WED THR FRI SAT".split(' ');
+var day = days[d.getDay()];
+var secondsToGo = 1200;
+
+console.log(day);
 
 var x = setInterval(function() {
   document.getElementById("timeDisplay").innerHTML = Math.floor((secondsToGo / 60)) + "m " + (secondsToGo % 60) + "s";
+
+  document.getElementById("dayDisplay").innerHTML = day;
 
   secondsToGo -=1;
   
