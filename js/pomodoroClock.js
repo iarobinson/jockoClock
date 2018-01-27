cebf12883a264eec73b71f7b5eaf6ca61fd577dc
Ian <ian (at) ianrobinson.net>
2018-01-26 17:09:53 -0800

    Typo on learn more footer link'

--- js/pomodoroClock.js
+++ js/pomodoroClock.js
@@ -4,26 +4,33 @@ var days = "SUN MON TUE WED THR FRI SAT".split(' ');
 var day = days[d.getDay()];
 
 // The number of seconds left on the pomodoro
-var secondsToGo = 1500;
 
-// Clock Operation and Display
-var x = setInterval(function() {
-  document.getElementById("timeDisplay").innerHTML = day + " " + Math.floor((secondsToGo / 60)) + ":" + displaySeconds(secondsToGo);
-  
-  secondsToGo -=1;
-  
-  if (secondsToGo < 0) {
-    clearInterval(x);
-    // Comment this out to avoid listening to the audio too much while programming
-    document.getElementById("warpath").play();
-    document.getElementById("timeDisplay").innerHTML = "<p id='expired'>EXPIRED<p>";
-    document.getElementById("dayDisplay").innerHTML = '';
-  }
-}, 1000);
+$(document).ready(function() {
+  var secondsToGo = 1500;
+
+  // Clock Operation and Display
+  var x = setInterval(function() {
+    // Renders time to watch display
+    document.getElementById("timeDisplay").innerHTML = day + "    " + Math.floor((secondsToGo / 60)) + ":" + displaySeconds(secondsToGo);
+    
+    // Decrements the time to go
+    secondsToGo -=1;
+    
+    // What to do when the minutes have counted down
+    if (secondsToGo < 0) {
+      clearInterval(x);
+      // Comment this out to avoid listening to the audio too much while programming
+      document.getElementById("warpath").play();
+      document.getElementById("timeDisplay").innerHTML = "<p id='expired'>EXPIRED<p>";
+      document.getElementById("dayDisplay").innerHTML = '';
+    }
+  }, 1000);
+});
 
 // Displays 00 rather than 01 for seconds and 04 seconds rather than 4
 function displaySeconds(seconds) {
   var sparSeconds = seconds % 60;
+
   if (sparSeconds === 0) {
     return '00';
   } else if (sparSeconds < 10) {
@@ -31,4 +38,12 @@ function displaySeconds(seconds) {
   } else {
     return sparSeconds;
   }
+}
+
+// Sets pomodoro timer to custom minutes
+function setTimer(string) {
+  console.log(string, "<-setTimer function");
+  var newtime = parseInt(string);
+  console.log(newtime);
+  secondsToGo = newTime * 60; 
 }
